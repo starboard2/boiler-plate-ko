ad11729cf170ad6059592089bd6fe3045c956d26
starboard2 <whdngus1207@gmail.com>
2022-05-17 22:08:43 +0900

    add Key

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://whjo:whjo1234@cluster0.mnqkt.mongodb.net/?retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+const bodyparser = require('body-parser');
+const { User } = require('./models/User');
+
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(config.mongoURI, {
     // useNewUrlparser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected....'))
-  .catch(err => console.log(err))
+  })
+  .then(() => console.log('MongoDB Connected....'))
+  .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!@@@');
+});
+
+app.post('/register', (req, res) => {
+  // 회원 가입 정보 Client -> DB Insert
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
